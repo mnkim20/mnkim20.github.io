d659e816a105bb7cbbca12c633bdde7613d6e30a
Mikela <mikelakim@gmail.com>
2018-11-01 02:04:43 -0700

    er

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,30 +1,19 @@
-var cnv;
-
-function centerCanvas() {
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
-  
-}
 
 function setup() {
-  cnv = createCanvas(1000, 400);
+  createCanvas(700, 400);
   centerCanvas();
-  background(50, 200, 230);
-}
+  background(238, 196, 255);
+  }
 
 function windowResized() {
   centerCanvas();
-}
-
-function draw() {
-  if (mouseX !== pmouseX || mouseY !== pmouseY) {
-    if (mouseIsPressed) {
-      fill(0,255,0);
-    }
-    else {
-      fill(random(250),random(200),random(100));
-    }
   }
-  rect(mouseX, mouseY, 80, 80);
-  //ellipse(mouseX, mouseY, 80, 80);
-}
+  
+function draw() {
+  if (mouseIsPressed) {
+  fill(104, 0, 145);
+  } else {
+  fill(random(300),random(800),random(600));
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+  }
