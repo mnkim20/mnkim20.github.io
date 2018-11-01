6d451007109bcf667b2285f583f80065068c64b8
Mikela <mikelakim@gmail.com>
2018-10-31 20:18:49 -0700

    er

--- p5/sketch.js
+++ p5/sketch.js
@@ -7,16 +7,24 @@ function centerCanvas() {
 }
 
 function setup() {
-  createCanvas(1000, 400);
+  cnv = createCanvas(1000, 400);
+  centerCanvas();
   background(240, 208, 255);
-  }
+}
+
+function windowResized() {
+  centerCanvas();
+}
+
   function draw() {
-    
+  if (mouseX !== pmouseX || mouseY !== pmouseY) {  
   if (mouseIsPressed) {
   fill(89, 9, 126);
   } 
   else {
   fill(random(300),random(700),random(200)); 
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  }
+  rect(mouseX, mouseY, 80, 80);
+  //ellipse(mouseX, mouseY, 80, 80);
   }
