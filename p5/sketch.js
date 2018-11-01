641b262e19def59b6808d0fd087af352e2c67b29
Mikela <mikelakim@gmail.com>
2018-11-01 01:45:37 -0700

    ll

--- p5/sketch.js
+++ p5/sketch.js
@@ -3,7 +3,7 @@ var cnv;
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
   var y = (windowHeight - height) / 2;
-  cnv.position(x, y);
+  cnv position(x, y);
 }
 
 function setup() {
